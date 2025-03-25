1527a0666adc1939f34eb7b917010a2975c86abb
JJ <pattarasaya.sr@kkumail.com>
2025-03-26 05:32:37 +0700

    bcryptjs

--- server/helpers/authHelper.js
+++ server/helpers/authHelper.js
@@ -1,25 +1,21 @@
-const bcrypt =require('bcrypt')
+const bcrypt = require('bcryptjs');
 
-///Hash function
-
-exports.hashPassword = (password) =>{
-  return new Promise((resolve,reject) => {
-    bcrypt.genSalt(10,(err,salt) => {
-      if(err) {
-        reject(err)
-      }
-      bcrypt.hash(password,salt,(err,hash) => {
-        if(err) {
-          reject(err);
-        }
-        resolve(hash);
-      })
-    } )
-  })
+// Hash function
+exports.hashPassword = async (password) => {
+  try {
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(password, salt);
+    return hash;
+  } catch (err) {
+    throw err;
+  }
 };
 
-//compare || decrypt function
-
-exports.comparePassword = (password,hashed) => {
-  return bcrypt.compare(password,hashed);
+// Compare function
+exports.comparePassword = async (password, hashed) => {
+  try {
+    return await bcrypt.compare(password, hashed);
+  } catch (err) {
+    throw err;
+  }
 };
